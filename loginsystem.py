2a365e2d76668b83192f3fa998de395a835ac064
kanikanailwal07-ui <kanikanailwal07@gmail.com>
2026-06-29 11:29:01 +0530

    login

---
+++ loginsystem.py
@@ -0,0 +1,20 @@
+'''login system with maximum 3 attempts'''
+#input username and password from user
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+#check if username and password are correct
+if username == "admin" and password =="python123":
+    print("Login successful")
+else:
+    print("Login failed")
+    #allow user to try again for maximum 3 attempts
+    for i in range(2):
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        if username == "admin" and password =="python123":
+            print("Login successful")
+            break
+        else:
+            print("Login failed")
+    else:
+        print("Maximum attempts reached. Try again later.")
