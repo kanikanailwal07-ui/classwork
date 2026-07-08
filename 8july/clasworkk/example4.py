6cc657313e011d8bea06a809d1cd163e40a628ef
kanikanailwal07-ui <kanikanailwal07@gmail.com>
2026-07-08 10:47:38 +0530

    nc

---
+++ 8july/clasworkk/example4.py
@@ -0,0 +1,15 @@
+#importing modeule
+import numericcalculation as nc
+a = 5
+b = 10
+print("sum of", a, "and", b, "is:", nc.calculate_sum(a, b))
+#-----------------------------
+print("difference of", a, "and", b, "is:", nc.calculate_difference(a, b))
+#-----------------------------
+print("multiplication of", a, "and", b, "is:", nc.calculate_multiplication(a, b))
+#-----------------------------
+print("division of", a, "and", b, "is:", nc.calculate_division(a, b))
+#-----------------------------
+print("remainder of", a, "and", b, "is:", nc.calculate_remainder(a, b))
+
+
