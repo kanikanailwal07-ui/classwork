a4faa322486f2a7f98f6043947dd396107b4049b
kanikanailwal07-ui <kanikanailwal07@gmail.com>
2026-07-08 10:37:19 +0530

    specific

---
+++ 8july/clasworkk/example3.py
@@ -0,0 +1,9 @@
+#importing module
+from numericcalculation import calculate_sum, calculate_difference
+a = 5
+b = 10
+print("sum of", a, "and", b, "is:", calculate_sum(a, b))
+#-----------------------------
+print("difference of", a, "and", b, "is:", calculate_difference(a, b))
+#-----------------------------
+print("multiplication of", a, "and", b, "is:", calculate_multiplication(a, b))
