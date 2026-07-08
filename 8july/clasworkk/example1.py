87c5505b4b12d7c0b2cfd30f128fe5b3e4081dc9
kanikanailwal07-ui <kanikanailwal07@gmail.com>
2026-07-08 10:23:03 +0530

    numeric

---
+++ 8july/clasworkk/example1.py
@@ -0,0 +1,16 @@
+#import module
+import numericcalculation
+#-----------------------------
+num1 = 5
+num2 = 10
+
+print("sum of", num1, "and", num2, "is:", numericcalculation.calculate_sum(num1, num2))
+#-----------------------------
+print("difference of", num1, "and", num2, "is:", numericcalculation.calculate_difference(num1, num2))
+#-----------------------------
+print("multiplication of", num1, "and", num2, "is:", numericcalculation.calculate_multiplication(num1, num2))
+#-----------------------------
+print("division of", num1, "and", num2, "is:", numericcalculation.calculate_division(num1, num2))
+#-----------------------------
+print("remainder of", num1, "and", num2, "is:", numericcalculation.calculate_remainder(num1, num2))
+
---
+++ 8july/clasworkk/numericcalculation.py
@@ -0,0 +1,15 @@
+def calculate_sum(num1,num2):
+    return num1 + num2
+#-----------------------------------
+def calculate_difference(num1,num2):
+    return num1 - num2
+#-----------------------------------
+def calculate_multiplication(num1,num2):
+    return num1 * num2
+#-----------------------------------
+def calculate_division(num1,num2):
+    return num1 / num2
+#-----------------------------------
+def calculate_remainder(num1,num2):
+    return num1 % num2
+
