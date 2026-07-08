471b2f9d83cdfdc5c5e26defe4f07227346603d3
kanikanailwal07-ui <kanikanailwal07@gmail.com>
2026-07-08 10:33:52 +0530

    import

---
+++ 8july/clasworkk/example2.py
@@ -0,0 +1,14 @@
+#importing module
+from numericcalculation import*
+#-----------------------------
+num1 = 5
+num2 = 10
+print("sum of", num1, "and", num2, "is:", calculate_sum(num1, num2))
+#-----------------------------
+print("difference of", num1, "and", num2, "is:", calculate_difference(num1, num2))
+#-----------------------------
+print("multiplication of", num1, "and", num2, "is:", calculate_multiplication(num1, num2))
+#-----------------------------
+print("division of", num1, "and", num2, "is:", calculate_division(num1, num2))
+#-----------------------------
+print("remainder of", num1, "and", num2, "is:", calculate_remainder(num1, num2))
